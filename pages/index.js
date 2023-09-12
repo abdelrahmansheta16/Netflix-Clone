1892f56ff40c7801c0e22f2b7b863a04b0e0c146
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-09-12 20:52:25 +0300

    First Commit

---
+++ pages/index.js
@@ -0,0 +1,88 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+import Banner from "../components/banner/banner";
+import NavBar from "../components/nav/navbar";
+
+import SectionCards from "../components/card/section-cards";
+
+import {
+  getVideos,
+  getPopularVideos,
+  getWatchItAgainVideos,
+  getMyList,
+} from "../lib/videos";
+import { redirectUser } from "../utils/redirectUser";
+
+export async function getServerSideProps(context) {
+  const { userId, token } = await redirectUser(context);
+  
+  const watchItAgainVideos = await getWatchItAgainVideos(userId, token);
+  const favouritedVideos = await getMyList(userId, token);
+
+  const disneyVideos = await getVideos("disney trailer");
+  const productivityVideos = await getVideos("Productivity");
+
+  const travelVideos = await getVideos("travel");
+
+  const popularVideos = await getPopularVideos();
+  return {
+    props: {
+      disneyVideos,
+      travelVideos,
+      productivityVideos,
+      popularVideos,
+      favouritedVideos,
+      watchItAgainVideos,
+    },
+  };
+}
+
+export default function Home({
+  disneyVideos,
+  travelVideos,
+  productivityVideos,
+  popularVideos,
+  favouritedVideos,
+  watchItAgainVideos,
+}) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={styles.main}>
+        <NavBar />
+        <Banner
+          videoId="4zH5iYM4wJo"
+          title="Clifford the red dog"
+          subTitle="a very cute dog"
+          imgUrl="/static/clifford.webp"
+        />
+
+        <div className={styles.sectionWrapper}>
+          <SectionCards title="Disney" videos={disneyVideos} size="large" />
+          {favouritedVideos.length > 0 ? <SectionCards
+            title="Your Favourites"
+            videos={favouritedVideos}
+            size="small"
+          /> : null}
+          {watchItAgainVideos.length > 0 ? <SectionCards
+            title="Watch it again"
+            videos={watchItAgainVideos}
+            size="small"
+          /> : null}
+          <SectionCards title="Travel" videos={travelVideos} size="small" />
+          <SectionCards
+            title="Productivity"
+            videos={productivityVideos}
+            size="medium"
+          />
+          <SectionCards title="Popular" videos={popularVideos} size="small" />
+        </div>
+      </div>
+    </div>
+  );
+}
